2e074cbb841cb96aee40dbb0b2138cfa90b9fb8e
Tomas Votruba <tomas.vot@gmail.com>
2026-06-23 16:43:00 +0200

    use stripos as insensitive

--- rector.php
+++ rector.php
@@ -46,7 +46,7 @@ return RectorConfig::configure()
     ])
     ->reportUnusedSkips()
     ->withTypeCoverageLevel(15)
-    ->withCodeQualityLevel(13)
+    ->withCodeQualityLevel(14)
     ->withSkip([
         Rector\Renaming\Rector\FuncCall\RenameFunctionRector::class,
         '*/Test/*',
