089d5952290018db38140dbd9518181c7497d672
fedys <miroslav.fedeles@gmail.com>
2026-06-26 13:55:30 +0200

    Use "customizations.vscode.settings" to open the readme file

--- .devcontainer/prebuild-project.sh
+++ .devcontainer/prebuild-project.sh
@@ -5,4 +5,3 @@ curl -fsSL https://ddev.com/install.sh | bash
 ddev start -y --skip-hooks
 ddev composer install --no-interaction
 ddev poweroff
-mkdir -p .vscode && echo '{"workbench.editorAssociations": {"*.md": "vscode.markdown.preview.editor"}}' > .vscode/settings.json
